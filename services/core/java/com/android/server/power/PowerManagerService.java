88ea62a4dfced5365210b071c80215785d733c0b
Cody Xie <xkd@rock-chips.com>
2015-03-02 19:39:50 +0800

    power: Add CPU boosting interface
         * Boosts CPU using Power HAL for the given duration.
         * Duration is given in microseconds.
         * Power HAL must implement POWER_HINT_CPU_BOOST.
    
        Change-Id: Ic79baf7e3d0f75483c2fe8a242b4c3d93368b68b
    
        Add CPU boosting hooks
    
         * Send boost hint during scrolling
         * Still relevant on even high-end hardware
    
        Change-Id: I72f2c2851b01fbceabddead03a4c3a7a0526ca61
    
        perf: Send a boost hint when a key on the navbar is pressed
    
         * A lot of stuff happens, especially when invoking recents.
         * Get ahead of the storm by sending a boost hint before
           kicking off the animations.
    
        Change-Id: I0cecd0c58f8bc8479ea44f63af796500d7b9d7d2
    
        perf: Send boost hint during initial launch
    
        Change-Id: I26afc909c35e514958894b0a07a650a20ce6e537
    
    Signed-off-by: Cody Xie <xkd@rock-chips.com>
    
    Conflicts:
    
    	tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
    
    Signed-off-by: Cody Xie <xkd@rock-chips.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -158,6 +158,9 @@ public final class PowerManagerService extends SystemService
     private static final int POWER_HINT_INTERACTION = 2;
     private static final int POWER_HINT_LOW_POWER = 5;
 
+    // Max time (microseconds) to allow a CPU boost for
+    private static final int MAX_CPU_BOOST_TIME = 5000000;
+
     private final Context mContext;
     private final ServiceThread mHandlerThread;
     private final PowerManagerHandler mHandler;
@@ -445,6 +448,7 @@ public final class PowerManagerService extends SystemService
     private static native void nativeSetInteractive(boolean enable);
     private static native void nativeSetAutoSuspend(boolean enable);
     private static native void nativeSendPowerHint(int hintId, int data);
+    private static native void nativeCpuBoost(int duration);
 
     public PowerManagerService(Context context) {
         super(context);
@@ -3139,6 +3143,20 @@ public final class PowerManagerService extends SystemService
                 return setLowPowerModeInternal(mode);
             } finally {
                 Binder.restoreCallingIdentity(ident);
+            }
+        }
+
+        /**
+         * Boost the CPU
+         * @param duration Duration to boost the CPU for, in milliseconds.
+         * @hide
+         */
+        @Override
+        public void cpuBoost(int duration) {
+            if (duration > 0 && duration <= MAX_CPU_BOOST_TIME) {
+                nativeCpuBoost(duration);
+            } else {
+                Log.e(TAG, "Invalid boost duration: " + duration);
             }
         }
 
--- tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
+++ tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
@@ -144,5 +144,10 @@ public class BridgePowerManager implements IPowerManager {
     @Override
     public void boostScreenBrightness(long time) throws RemoteException {
         // pass for now.
+	}
+
+	@Override
+    public void cpuBoost(int duration) throws RemoteException {
+        // pass for now
     }
 }
