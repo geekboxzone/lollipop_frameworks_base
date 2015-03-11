9310af974f9ccb9ebe782374a0a10524168a25ea
cw <cw@rock-chips.com>
2015-03-11 17:53:29 +0800

    Revert "ethernet: support statusbar ethernet connect icon"
    This reverts commit 08041edc2cefd03773c97036febdc6e9f2654419.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -34,7 +34,6 @@ public interface NetworkController {
                 boolean isDataTypeIconWide);
         void onAirplaneModeChanged(boolean enabled);
         void onMobileDataEnabled(boolean enabled);
-        //void onEthernetModeChanged(boolean enabled);
     }
 
     void addAccessPointCallback(AccessPointCallback callback);
