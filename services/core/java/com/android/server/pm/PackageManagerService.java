5c275423b718c2b2f8b270edef1bef7302bc017a
Weilong Gao <gwl@rock-chips.com>
2015-06-18 11:35:33 +0800

    Revert "framework: workaround to fetch the correct abi for the bundled-apps with cluster installation"
    This reverts commit a328645f60faf029f7f391187b61b916d92e63f8.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -6813,12 +6813,10 @@ public class PackageManagerService extends IPackageManager.Stub {
 
         final boolean has64BitLibs;
         final boolean has32BitLibs;
-        final File clusterRootDir;
         if (isApkFile(codeFile)) {
             // Monolithic install
             has64BitLibs = (new File(apkRoot, new File(LIB64_DIR_NAME, apkName).getPath())).exists();
             has32BitLibs = (new File(apkRoot, new File(LIB_DIR_NAME, apkName).getPath())).exists();
-            clusterRootDir = null;
         } else {
             // Cluster install
             final File rootDir = new File(codeFile, LIB_DIR_NAME);
@@ -6836,7 +6834,6 @@ public class PackageManagerService extends IPackageManager.Stub {
             } else {
                 has32BitLibs = false;
             }
-            clusterRootDir = rootDir;
         }
 
         if (has64BitLibs && !has32BitLibs) {
@@ -6874,16 +6871,6 @@ public class PackageManagerService extends IPackageManager.Stub {
         } else {
             pkg.applicationInfo.primaryCpuAbi = null;
             pkg.applicationInfo.secondaryCpuAbi = null;
-            if (clusterRootDir != null) {
-                if ((VMRuntime.is64BitInstructionSet(getPreferredInstructionSet()))
-                    && ((new File(clusterRootDir, new String("arm64"))).exists())) {
-                    pkg.applicationInfo.primaryCpuAbi = new String("arm64-v8a");
-                } else if ((new File(clusterRootDir, (new String("arm")))).exists()) {
-                    pkg.applicationInfo.primaryCpuAbi = new String("armeabi-v7a");
-                } else if ((new File(clusterRootDir, (new String("arm64")))).exists()) {
-                    pkg.applicationInfo.primaryCpuAbi = new String("arm64-v8a");
-                }
-            }
         }
     }
 
