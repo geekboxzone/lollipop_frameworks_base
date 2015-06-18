01c54b96da1a2a860e51b50ecab1d4f02046176e
Weilong Gao <gwl@rock-chips.com>
2015-06-18 11:46:14 +0800

    Revert "Revert "ActivityManagerService:fix display lock screen when incoming calls""
    This reverts commit c0bc94e49b95be368fcf0db011e5e2704bfe0979.

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -237,6 +237,7 @@ import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import android.app.KeyguardManager;
+import android.telecom.TelecomManager;
 
 public final class ActivityManagerService extends ActivityManagerNative
         implements Watchdog.Monitor, BatteryStatsImpl.BatteryCallback {
@@ -16837,7 +16838,8 @@ public final class ActivityManagerService extends ActivityManagerNative
             if (starting != null) {
 				if(mKeyguardManager.isKeyguardLocked()){
 					Slog.d("ljh","-----------------------starting.info.screenOrientation="+starting.info.screenOrientation);
-					if(starting.info.screenOrientation==ActivityInfo.SCREEN_ORIENTATION_PORTRAIT||starting.info.screenOrientation==ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE){
+					TelecomManager telecomManager = (TelecomManager) mContext.getSystemService(Context.TELECOM_SERVICE);
+					if(starting.info.screenOrientation==ActivityInfo.SCREEN_ORIENTATION_PORTRAIT||starting.info.screenOrientation==ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE || (telecomManager!=null&&telecomManager.isRinging())){
                         //Slog.d("ljh","do not change------------");
 				    }else{
 	                     //Slog.d("ljh","force change---------------");
