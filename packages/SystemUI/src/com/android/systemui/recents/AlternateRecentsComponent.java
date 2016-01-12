74ff9f90db6743610518107d092942557a017c98
zj <zj@rock-chips.com>
2016-01-12 15:30:44 +0800

    From 5e834aed18b70f2b2af86c5a4a0a3b6873aea5f4 Mon Sep 17 00:00:00 2001 From: Winson <winsonc@google.com> Date: Tue, 10 Nov 2015 16:19:38 -0800 Subject: [PATCH 1/2] DO NOT MERGE Ensure that the device is provisioned before  showing Recents.
     Bug: 25476219
     Change-Id: I5bb9cca74790521de71c0037b4f2421c3d21b3f6
     (cherry picked from commit 16a76dadcc23a13223e9c2216dad1fe5cad7d6e1)
    
    Signed-off-by: zj <zj@rock-chips.com>

--- packages/SystemUI/src/com/android/systemui/recents/AlternateRecentsComponent.java
+++ packages/SystemUI/src/com/android/systemui/recents/AlternateRecentsComponent.java
@@ -36,6 +36,7 @@ import android.graphics.Rect;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -244,6 +245,12 @@ public class AlternateRecentsComponent implements ActivityOptions.OnAnimationSta
     /** Shows the Recents. */
     @ProxyFromPrimaryToCurrentUser
     public void onShowRecents(boolean triggeredFromAltTab) {
+        // Ensure the device has been provisioned before allowing the user to interact with
+        // recents
+        if (!isDeviceProvisioned()) {
+            return;
+        }
+
         if (mSystemServicesProxy.isForegroundUserOwner()) {
             showRecents(triggeredFromAltTab);
         } else {
@@ -266,6 +273,13 @@ public class AlternateRecentsComponent implements ActivityOptions.OnAnimationSta
     /** Hides the Recents. */
     @ProxyFromPrimaryToCurrentUser
     public void onHideRecents(boolean triggeredFromAltTab, boolean triggeredFromHomeKey) {
+         // Ensure the device has been provisioned before allowing the user to interact with
+        // recents
+        if (!isDeviceProvisioned()) {
+            return;
+        }
+
+       
     	//Add by huang for win recents
         if (mContext.getResources().getConfiguration().enableMultiWindow()||mSystemServicesProxy.isForegroundUserOwner()) {
             hideRecents(triggeredFromAltTab, triggeredFromHomeKey);
@@ -294,6 +308,12 @@ public class AlternateRecentsComponent implements ActivityOptions.OnAnimationSta
     /** Toggles the Recents activity. */
     @ProxyFromPrimaryToCurrentUser
     public void onToggleRecents() {
+        // Ensure the device has been provisioned before allowing the user to interact with
+        // recents
+        if (!isDeviceProvisioned()) {
+            return;
+        }
+
         if (mSystemServicesProxy.isForegroundUserOwner()) {
             toggleRecents();
         } else {
@@ -315,6 +335,12 @@ public class AlternateRecentsComponent implements ActivityOptions.OnAnimationSta
     /** Preloads info for the Recents activity. */
     @ProxyFromPrimaryToCurrentUser
     public void onPreloadRecents() {
+        // Ensure the device has been provisioned before allowing the user to interact with
+        // recents
+        if (!isDeviceProvisioned()) {
+            return;
+        }
+
         if (mSystemServicesProxy.isForegroundUserOwner()) {
             preloadRecents();
         } else {
@@ -407,10 +433,22 @@ public class AlternateRecentsComponent implements ActivityOptions.OnAnimationSta
     }
 
     public void onShowNextAffiliatedTask() {
+        // Ensure the device has been provisioned before allowing the user to interact with
+        // recents
+        if (!isDeviceProvisioned()) {
+            return;
+        }
+
         showRelativeAffiliatedTask(true);
     }
 
     public void onShowPrevAffiliatedTask() {
+        // Ensure the device has been provisioned before allowing the user to interact with
+        // recents
+        if (!isDeviceProvisioned()) {
+            return;
+        }
+
         showRelativeAffiliatedTask(false);
     }
 
@@ -760,6 +798,14 @@ public class AlternateRecentsComponent implements ActivityOptions.OnAnimationSta
         RecentsTaskLoadPlan plan = sInstanceLoadPlan;
         sInstanceLoadPlan = null;
         return plan;
+    }
+
+    /**
+     * @return whether this device is provisioned.
+     */
+    private boolean isDeviceProvisioned() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.DEVICE_PROVISIONED, 0) != 0;
     }
 
     /**** OnAnimationStartedListener Implementation ****/
